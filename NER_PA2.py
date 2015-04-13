3874cd09fb9cfe8dd80357dcd58211a693be86ff
sr925 <srinisha@dhcp-rhodes-3084.redrover.cornell.edu>
2015-04-13 19:13:35 -0400

    Gets Entity if expected entity is not found POS tagging is done

--- NER_PA2.py
+++ NER_PA2.py
@@ -2,19 +2,34 @@
 # python interface for stanford NER - https://github.com/dat/stanford-ner
 # using pyner - python interface to stanford NER
 import ner
-
+import nltk 
+from nltk import word_tokenize
 def queryForEntity1(expectedEntity,passage):
     tagger = ner.SocketNER(host='localhost', port=8081) # requires server to be started
     answer=tagger.get_entities(passage)
     answers=[]
     for i,pair in enumerate(answer):
         if(pair[1]==expectedEntity):
-            answers.append(answer[i])       
+            answers.append(answer[i]) 
     return answers
 
-answersList=queryForEntity1("PERSON","Rami Eid is studying at Stony Brook University in NY") # person is not tagged while using pyner
+answersList=queryForEntity1("PERSON","Ezra is great") # person is not tagged while using pyner
 print "answer using python interface & NER" 
 print answersList
+if answersList==[]:
+    # Using POS tagging to get noun phrase
+    print "ner is of no use"
+    answer = word_tokenize("Ezra is great")
+    answers=nltk.pos_tag(answer)
+    print answers
+    for i,pair in enumerate(answers):
+        if(pair[1]=="NNP"):
+            answersList.append(answer[i]) 
+    print "noun pharases"
+    print answersList
+
+
+# !!!!!!!!!!OR!!!!!! 
 
 #or  using nltk stanford NER
 
@@ -22,9 +37,25 @@ from nltk.tag.stanford import NERTagger
 def queryForEntity2(expectedEntity,passage):
     st = NERTagger('/Users/srinisha/Downloads/stanford-ner-2014-06-16/classifiers/english.all.3class.distsim.crf.ser.gz','/Users/srinisha/Downloads/stanford-ner-2014-06-16/stanford-ner.jar') 
     answer=st.tag(passage.split()) 
+    print answer
     answers=[]
     for i,pair in enumerate(answer):
         if(pair[1]==expectedEntity):
-            answers.append(answer[i])       
+            answers.append(answer[i])   
     return answers
 
+
+answersList=queryForEntity2("PERSON","Ezra is great")
+print "answer using nltk.tag.stanford NERTagger" 
+print answersList
+if answersList==[]:
+    # Using POS tagging to get noun phrase
+    print "ner is of no use"
+    answer = word_tokenize("Ezra is great")
+    answers=nltk.pos_tag(answer)
+    print answers
+    for i,pair in enumerate(answers):
+        if(pair[1]=="NNP"):
+            answersList.append(answer[i]) 
+    print "noun pharases"
+    print answersList
