3856c24989058ab4c9971efd83181794d613e23e
sr925 <srinisha@dhcp-rhodes-3084.redrover.cornell.edu>
2015-04-13 19:59:13 -0400

    Use queryForEntity1(expectedEntity,passage) to get answers

--- NER_PA2.py
+++ NER_PA2.py
@@ -1,37 +1,16 @@
 # nltk stanford NER -http://stackoverflow.com/questions/18371092/stanford-named-entity-recognizer-ner-functionality-with-nltk
 # python interface for stanford NER - https://github.com/dat/stanford-ner
 # using pyner - python interface to stanford NER
+
+#----------
+
 import ner
 import nltk 
 from nltk import word_tokenize
-def queryForEntity1(expectedEntity,passage):
-    tagger = ner.SocketNER(host='localhost', port=8081) # requires server to be started
-    answer=tagger.get_entities(passage)
-    answers=[]
-    for i,pair in enumerate(answer):
-        if(pair[1]==expectedEntity):
-            answers.append(answer[i]) 
-    return answers
-
-answersList=queryForEntity1("PERSON","Ezra is great") # person is not tagged while using pyner
-print "answer using python interface & NER" 
-print answersList
-if answersList==[]:
-    # Using POS tagging to get noun phrase
-    print "ner is of no use"
-    answer = word_tokenize("Ezra is great")
-    answers=nltk.pos_tag(answer)
-    print answers
-    for i,pair in enumerate(answers):
-        if(pair[1]=="NNP"):
-            answersList.append(answer[i]) 
-    print "noun pharases"
-    print answersList
 
 
-# !!!!!!!!!!OR!!!!!! 
-
-#or  using nltk stanford NER
+#or using nltk stanford NER
+# Use this for now -- take a few seconds (a little slow)
 
 from nltk.tag.stanford import NERTagger
 def queryForEntity2(expectedEntity,passage):
@@ -39,15 +18,17 @@ def queryForEntity2(expectedEntity,passage):
     answer=st.tag(passage.split()) 
     print answer
     answers=[]
-    for i,pair in enumerate(answer):
-        if(pair[1]==expectedEntity):
-            answers.append(answer[i])   
+    for j,currentExpectedEntity in enumerate(expectedEntity):
+        for i,pair in enumerate(answer):
+            if(pair[1]==currentExpectedEntity):
+                answers.append(answer[i])   
     return answers
 
-
-answersList=queryForEntity2("PERSON","Ezra is great")
+expectedEntity=["PERSON","LOCATION"]
+answersList=queryForEntity2(expectedEntity,"Ezra is great in Ithaca")
 print "answer using nltk.tag.stanford NERTagger" 
 print answersList
+
 if answersList==[]:
     # Using POS tagging to get noun phrase
     print "ner is of no use"
@@ -59,3 +40,38 @@ if answersList==[]:
             answersList.append(answer[i]) 
     print "noun pharases"
     print answersList
+
+# !!!!!!!!!!OR!!!!!! 
+
+
+
+#def queryForEntity1(expectedEntity,passage):
+#    tagger = ner.SocketNER(host='localhost', port=8081) # requires server to be started
+#    answer=tagger.get_entities(passage)
+#    print answer
+#    answers=[]
+#    for j,currentExpectedEntity in enumerate(expectedEntity):
+#        for i,pair in enumerate(answer):
+         # pair is not working properly for some entities like location
+#            if(pair[1]==currentExpectedEntity):
+#                answers.append(answer[i]) 
+#    return answers
+#    
+#expectedEntity=["PERSON","LOCATION"]
+#answersList=queryForEntity1(expectedEntity,"Ezra is great in Ithaca")
+#print "answer using python interface & NER" 
+#print answersList
+#if answersList==[]:
+#    # Using POS tagging to get noun phrase
+#    print "ner is of no use"
+#    answer = word_tokenize("Ezra is great")
+#    answers=nltk.pos_tag(answer)
+#    print answers
+#    for i,pair in enumerate(answers):
+#        if(pair[1]=="NNP"):
+#            answersList.append(answer[i]) 
+#    print "noun pharases"
+#    print answersList
+
+
+
