0992cd6a960ea773c614785bb14aa76a234a5053
sr925 <srinisha@Srinishas-MacBook-Air.local>
2015-04-17 04:55:36 -0400

    consistent copy with all changes as of now included

--- similarity.py
+++ similarity.py
@@ -4,16 +4,20 @@ import os
 import datetime
 from operator import itemgetter
 from nltk.corpus import stopwords
+import nltk 
+import collections
+from nltk import word_tokenize
+from nltk.tag.stanford import NERTagger
 
-entity_labels = {"HOW": ["LOCATION","PERSON", "TIME", "DATE", "MONEY", "PERCENT"], "WHAT": ["LOCATION","PERSON", "TIME", "DATE", "MONEY", "PERCENT"],"WHERE": ["LOCATION"], "WHO": ["PERSON", "ORGANIZATION"], "WHEN": ["TIME", "DATE"], "HOW MANY": ["COUNT","MONEY","PERCENT"],"WHICH":["LOCATION","PERSON", "TIME", "DATE", "MONEY", "PERCENT"]}
+entity_labels = {"How": ["LOCATION","PERSON", "TIME", "DATE", "MONEY", "PERCENT"], "What": ["LOCATION","PERSON", "TIME", "DATE", "MONEY", "PERCENT"],"WHERE": ["LOCATION"], "Who": ["PERSON", "ORGANIZATION"], "When": ["TIME", "DATE"], "How many": ["COUNT","MONEY","PERCENT"],"Which":["LOCATION","PERSON", "TIME", "DATE", "MONEY", "PERCENT"]}
+dict_phrases = {}
 
 
 def similarity(query_dict,top_docs_dict):
     query_no = query_dict.keys()
-    dict_phrases = {}
+
     for i in range(len(query_no)):
         list_scores = []
-        
         query = query_no[i]
         doc_dict = top_docs_dict[query]
         query_text = query_dict[query]
@@ -24,7 +28,8 @@ def similarity(query_dict,top_docs_dict):
         #return list of ngrams here and iterate over each n-gram
         keys_list_docs = doc_dict.keys()
         fwrite = open("tempfile", "w+")
-       
+            
+        
         for l in range(len(doc_dict)):
             n_grams = doc_dict[keys_list_docs[l]]
             for n in range(len(n_grams)):
@@ -32,12 +37,12 @@ def similarity(query_dict,top_docs_dict):
                 doc_tf_dict = find_tf(n_grams[n])
                 doc_normalized = cosine_normalize(doc_tf_dict)
                 score = calculate_dot_product(query_normalized,doc_normalized)
-                print n_grams[n],score 
+                #print n_grams[n],score 
                 dict_scores['phrase'] = n_grams[n]
                 dict_scores['score'] = score
                 list_scores.append(dict_scores)
         newlist = sorted(list_scores, key=itemgetter('score'), reverse=True) 
-        dict_phrases[i] = newlist
+        dict_phrases[query] = newlist
         for item in newlist:
             fwrite.write("%s\n" % item)      
         fwrite.close()
@@ -105,8 +110,13 @@ def cosine_normalize(dict_tf):
         sum_denominator = sum_denominator + math.pow(dict_tf.get(keys[k]),2)
     denominator = math.sqrt(sum_denominator)
     
+ 
+    
     for k in range(len(keys)):
         value = dict_tf.get(keys[k])
+#        print keys[k]
+#        print value
+#        print denominator
         dict_tf[keys[k]] = value/denominator
         
     return dict_tf
@@ -135,8 +145,9 @@ def getquerydict(questions_filename):
                 line=" ".join([w for w in line.split(" ") if not w in stop])
                 newline = re.compile(r'(\n)', re.UNICODE)
                 line = newline.sub('',line)
-                query_dict.update({qnum[1]:line})
-                
+                punctuation = re.compile(r'[\?."\',\(\)&/:]+', re.UNICODE)                
+                line = punctuation.sub('',line)
+                query_dict.update({qnum[1]:line})          
     return query_dict
 
 def preprocessing(finalstr):
@@ -159,29 +170,17 @@ def preprocessing(finalstr):
 
 def getngrams(ngramterms):
     ngram_list = []
-    for i in range(0, len(ngramterms) - 9):
-            if ngramterms[i] != "." and ngramterms[i+1] !="." and ngramterms[i+2] != "." and ngramterms[i+3] != "." and ngramterms[i+4] != "." and ngramterms[i+5] != "." and ngramterms[i+6] != "." and ngramterms[i+7] != "." and ngramterms[i+8] != ".":
-                ngram_list.append(ngramterms[i] + " " + ngramterms[i + 1] + " " + ngramterms[i + 2] + " " + ngramterms[i + 3] + " " + ngramterms[i + 4] + " " + ngramterms[i + 5] + " " + ngramterms[i + 6] + " " + ngramterms[i + 7] + " " + ngramterms[i + 8] + " " + ngramterms[i + 9])
-            '''
-            else :
-                if ngramterms[i] != "." :
-                    if ngramterms[i+1] == "." :
-                        ngram_list.append(ngramterms[i] + " " + ngramterms[i + 1])
-                    elif ngramterms[i+2] == "." :
-                        ngram_list.append(ngramterms[i] + " " + ngramterms[i + 1] + " " + ngramterms[i + 2])
-                    elif ngramterms[i+3] == "." :
-                        ngram_list.append(ngramterms[i] + " " + ngramterms[i + 1] + " " + ngramterms[i + 2] + " " + ngramterms[i + 3])
-                    elif ngramterms[i+4] == "." :
-                        ngram_list.append(ngramterms[i] + " " + ngramterms[i + 1] + " " + ngramterms[i + 2] + " " + ngramterms[i + 3] + " " + ngramterms[i + 4])
-                    elif ngramterms[i+5] == "." :
-                        ngram_list.append(ngramterms[i] + " " + ngramterms[i + 1] + " " + ngramterms[i + 2] + " " + ngramterms[i + 3] + " " + ngramterms[i + 4] + " " + ngramterms[i + 5])
-                    elif ngramterms[i+6] == "." :
-                        ngram_list.append(ngramterms[i] + " " + ngramterms[i + 1] + " " + ngramterms[i + 2] + " " + ngramterms[i + 3] + " " + ngramterms[i + 4] + " " + ngramterms[i + 5] + " " + ngramterms[i + 6])
-                    elif ngramterms[i+7] == "." :
-                        ngram_list.append(ngramterms[i] + " " + ngramterms[i + 1] + " " + ngramterms[i + 2] + " " + ngramterms[i + 3] + " " + ngramterms[i + 4] + " " + ngramterms[i + 5] + " " + ngramterms[i + 6] + " " + ngramterms[i + 7])
-                    elif ngramterms[i+8] == "." :
-                        ngram_list.append(ngramterms[i] + " " + ngramterms[i + 1] + " " + ngramterms[i + 2] + " " + ngramterms[i + 3] + " " + ngramterms[i + 4] + " " + ngramterms[i + 5] + " " + ngramterms[i + 6] + " " + ngramterms[i + 7] + " " + ngramterms[i + 8])
-            '''
+    for eachTerm in ngramterms :
+        tempTerm = eachTerm.split()
+        if len(tempTerm)> 9  :
+             for i in range(0, len(tempTerm) - 9) :
+                 ngram_list.append(tempTerm[i] + " " + tempTerm[i + 1] + " " + tempTerm[i + 2] + " " + tempTerm[i + 3] + " " + tempTerm[i + 4] + " " + tempTerm[i + 5] + " " + tempTerm[i + 6] + " " + tempTerm[i + 7] + " " + tempTerm[i + 8] + " " + tempTerm[i + 9])
+        else :
+             if len(tempTerm) != 0 :
+                 temp = tempTerm[0]
+                 for i in range(1, len(tempTerm)) :
+                     temp += " " + tempTerm[i]
+                 ngram_list.append(temp)
     return ngram_list
 
 def getTopDocsDict(pathTopDocs):
@@ -216,7 +215,7 @@ def getTopDocsDict(pathTopDocs):
             whitespace = re.compile(r'(\s)+', re.UNICODE)
             tempCompText = whitespace.sub(' ', tempCompText)
     
-            ngramterms = tempCompText.split()
+            ngramterms = tempCompText.split(".")
             ngram_list = getngrams(ngramterms)
     
             docs_dict.update({index:ngram_list})
@@ -226,10 +225,106 @@ def getTopDocsDict(pathTopDocs):
     
     return top_docs_dict
 
+
+# nltk stanford NER
+# Use this for now -- take a few seconds (a little slow)
+
+# nltk stanford NER -http://stackoverflow.com/questions/18371092/stanford-named-entity-recognizer-ner-functionality-with-nltk
+
+
+def queryForEntity(expectedEntity,passage,pathtoClassifier,pathtoNerjar):
+    st = NERTagger(pathtoClassifier,pathtoNerjar) 
+    answer=st.tag(passage.split()) 
+    answers=[]
+    for j,currentExpectedEntity in enumerate(expectedEntity):
+        for i,pair in enumerate(answer):
+            if(pair[1]==currentExpectedEntity):
+                answers.append(answer[i])   
+    return answers
+
+
+def getAnswers(pathtoClassifier,pathtoNerjar,query_dict):
+    query_dict=collections.OrderedDict(sorted(query_dict.items()))
+    for query in query_dict: # for every query
+        cnt=0
+        # entity_labels doesn't yet have entries like What's  
+        Query=query_dict[query]
+        QueryNo= query;
+        print "QueryNo"
+        print QueryNo
+        print "Current Query"
+        print Query
+        
+     
+
+        testHowMany = re.compile("How many") 
+        testHow=re.compile("How") 
+        testWhen=re.compile("When")
+        testWhich=re.compile("Which")
+        testWho=re.compile("Who")
+        testWhat=re.compile("What")
+        testWhere=re.compile("Where")
+        if testHow.match(Query):
+            expectedEntity=entity_labels["How"]
+        if testHowMany.match(Query):
+            expectedEntity=entity_labels["How many"]        
+        if testWhen.match(Query):
+            expectedEntity=entity_labels["When"]
+        if testWhich.match(Query):
+            expectedEntity=entity_labels["Which"]
+        if testWho.match(Query):
+            expectedEntity=entity_labels["Who"]
+        if testWhat.match(Query):
+            expectedEntity=entity_labels["What"]
+        if testWhere.match(Query):
+            expectedEntity=entity_labels["Where"]
+        print "expectedEntity"
+        print expectedEntity
+        #print "New query "
+        list_scores=dict_phrases[QueryNo]
+        
+        for currDict in list_scores: 
+            if cnt>10:
+                break
+            else:
+                currpassage=currDict['phrase']
+                print "current passage (ngram) "
+                print currpassage
+                answersList=[]
+                if expectedEntity!=[]:
+                    answersList=queryForEntity(expectedEntity,currpassage,pathtoClassifier,pathtoNerjar)
+                    #if len(answersList)!=0:
+                        #print "answer found shouldn't go to POS"
+                #else:
+                    #print "Couldnt find any matching entries in entity_labels dictory so didnt go for queryForEntity function"           
+                if len(answersList)==0:
+                    # since no answer was found from any of the exoected entities noun phrase extraction was done
+                    # Using POS tagging to get noun phrase
+                    #print "when answer list should be empty to come here"
+                    #print "ner is of no use doing POS tagging to get noun phrase"
+                    answer = word_tokenize(currpassage)
+                    answers=nltk.pos_tag(answer)
+                    for i,pair in enumerate(answers):
+                        if(pair[1]=="NNP"):
+                            answersList.append(answer[i]) 
+                            cnt=cnt+1
+                            #print cnt
+                    print "answer found from noun pharases"
+                    print answersList                    
+                else:                     
+                    cnt=cnt+1  
+                    #print cnt
+                    print "answer using nltk.tag.stanford NERTagger" 
+                    print answersList
+
+        
+        
+    
+
 def main():
     print "Process started", datetime.datetime.now().time()
-    questions_filename = "questions.txt"
-    pathTopDocs = "dev1/"  
+    questions_filename = "/Users/srinisha/Downloads/pa2-release/qadata/dev/questions.txt"
+    pathTopDocs = "/Users/srinisha/Downloads/pa2-release/topdocs/dev/"  
     
     query_dict = getquerydict(questions_filename)
     print "Query Dictionary Generated", datetime.datetime.now().time()
@@ -239,6 +334,10 @@ def main():
     
     similarity(query_dict,top_docs_dict)
     print "Process completed", datetime.datetime.now().time()
+    
+    pathtoClassifier='/Users/srinisha/Downloads/stanford-ner-2014-06-16/classifiers/english.all.3class.distsim.crf.ser.gz'
+    pathtoNerjar='/Users/srinisha/Downloads/stanford-ner-2014-06-16/stanford-ner.jar'
+    getAnswers(pathtoClassifier,pathtoNerjar,query_dict)
     
 if __name__ == "__main__":
     main()   
